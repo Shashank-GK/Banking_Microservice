1fcee626e38f9a27d051e2391e07229793233ad2
iVoyantShashank <shashankg@ivoyant.com>
2025-02-10 11:57:34 +0530

    Commiting 3 Services with out external config

---
+++ accounts/src/main/java/com/ivoyant/accounts/mapper/AccountsMapper.java
@@ -0,0 +1,22 @@
+package com.ivoyant.accounts.mapper;
+
+import com.ivoyant.accounts.dto.AccountsDto;
+import com.ivoyant.accounts.entity.Accounts;
+
+public class AccountsMapper {
+
+    public static AccountsDto mapToAccountsDto(Accounts accounts, AccountsDto accountsDto) {
+        accountsDto.setAccountNumber(accounts.getAccountNumber());
+        accountsDto.setAccountType(accounts.getAccountType());
+        accountsDto.setBranchAddress(accounts.getBranchAddress());
+        return accountsDto;
+    }
+
+    public static Accounts mapToAccounts(AccountsDto accountsDto, Accounts accounts) {
+        accounts.setAccountNumber(accountsDto.getAccountNumber());
+        accounts.setAccountType(accountsDto.getAccountType());
+        accounts.setBranchAddress(accountsDto.getBranchAddress());
+        return accounts;
+    }
+
+}
---
+++ cards/src/test/java/com/ivoynat/cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoynat.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
